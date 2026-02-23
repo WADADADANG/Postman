8163fdb3072602718de6f176ab143a97c958cc71
WADADADANG <kitsada19972540@gmail.com>
2026-02-24 05:49:41 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 3001 })
+        await fastify.listen({ port: process.env.PORT || 3001 })
         console.log('Server is running at http://localhost:3000')
     } catch (err) {
         fastify.log.error(err)
